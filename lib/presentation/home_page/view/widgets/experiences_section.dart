1c04b5bb467b4c9762f3eb1feb6d6421694539f6
repsajanboy <janboym@gmail.com>
2025-02-03 22:08:55 +0800

    Experiences Section

---
+++ lib/presentation/home_page/view/widgets/experiences_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExperiencesSection extends StatelessWidget {
+  const ExperiencesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
